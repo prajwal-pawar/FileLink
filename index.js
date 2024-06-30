b9543021890581c6956945da12db99a409f03838
Prajwal Pawar <prajwalpawar2303@gmail.com>
2024-06-30 13:48:30 +0530

    add configs

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const app = express();
 
 const PORT = 8000;
 
+// body parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// routes
+app.use("/", require("./routes"));
+
 // start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
