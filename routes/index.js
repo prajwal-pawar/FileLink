8c775e6028f9d0545742841133c8b1ad64f944ea
Prajwal Pawar <prajwalpawar2303@gmail.com>
2024-06-30 15:44:26 +0530

    add upload route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const upload = require("../configs/multer");
 const homeController = require("../controllers/home_controller");
 
 router.get("/", homeController.home);
+
+router.post("/upload", upload.single("file"), homeController.upload);
 
 module.exports = router;
